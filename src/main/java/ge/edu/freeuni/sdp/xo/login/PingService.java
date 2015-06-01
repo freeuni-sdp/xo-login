1bd9fbe9bb4b95a3cbb23d4a5b2379b834e76bef
slezh12 <slezh12@freeuni.edu.ge>
2015-06-01 15:22:36 +0300

    created project,added ping service and it's test

---
+++ src/main/java/ge/edu/freeuni/sdp/xo/login/PingService.java
@@ -0,0 +1,17 @@
+package ge.edu.freeuni.sdp.xo.login;
+
+
+import javax.ws.rs.*;
+import javax.ws.rs.core.*;
+
+@Path("ping")
+@Consumes( { MediaType.APPLICATION_JSON})
+@Produces( { MediaType.APPLICATION_JSON})
+public class PingService {
+
+  @GET
+  public Response get() {
+    return Response.ok().build();
+  }
+
+}
