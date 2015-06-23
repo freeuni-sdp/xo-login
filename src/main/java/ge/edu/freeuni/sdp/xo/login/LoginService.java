d590bb413665b4c1cbb75df57db0b1964083cea4
gkiko <gkiko10@freeuni.edu.ge>
2015-06-23 16:06:42 +0400

    fixed failing tests

--- src/main/java/ge/edu/freeuni/sdp/xo/login/LoginService.java
+++ src/main/java/ge/edu/freeuni/sdp/xo/login/LoginService.java
@@ -1,7 +1,5 @@
 package ge.edu.freeuni.sdp.xo.login;
 
-
-import java.util.HashMap;
 import java.util.UUID;
 
 import javax.ws.rs.*;
@@ -13,46 +11,9 @@ import javax.ws.rs.core.Response.Status;
 @Produces( { MediaType.APPLICATION_JSON})
 public class LoginService {
 
-	private static class KVStore{
-		private KVStore kvStore;
-
-		private static HashMap<String, UserInformation> users = new HashMap<>();  		// username -> data
-		private static HashMap<String, UserInformation> tokenCheck = new HashMap<>(); 	// token -> data
-
-		static {
-			UserInformation newUser = new UserInformation();
-			newUser.email = "slezh12@freeuni.edu.ge";
-			newUser.password = "blabla";
-			newUser.username = "sandro";
-
-			UserInformation newUser1 = new UserInformation();
-			newUser1.email = "agoro12@freeuni.edu.ge";
-			newUser1.password = "blabla";
-			newUser1.username = "anna";
-
-			users.put(newUser.username, newUser);
-			users.put(newUser1.username, newUser1);
-			tokenCheck.put("some_token", newUser);
-			// newUser1 without token
-		}
-
-		public UserInformation getByName(String k1) {
-			return users.get(k1);
-		}
-
-		public UserInformation getByToken(String k2) {
-			return tokenCheck.get(k2);
-		}
-
-		public void putByToken(String k2, UserInformation userInfo) {
-			tokenCheck.put(k2, userInfo);
-		}
+	private FakeKVStore getStore() {
+		return new FakeKVStore();
 	}
-
-	private KVStore getStore() {
-		return new KVStore();
-	}
-
 
 	@PUT
 	public Token loginUser(LoginInformation userInfo){
